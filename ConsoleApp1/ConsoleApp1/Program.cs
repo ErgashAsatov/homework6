d258abef1b1d9dbec263d4641a9c0bbef0fab7c1
ergash723 <125293023+ergash723@users.noreply.github.com>
2023-06-07 19:09:10 +0500

    task 4

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,16 +1,34 @@
-﻿namespace ConsoleApp1
+﻿using Microsoft.VisualBasic.FileIO;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string path = @"D:\hello c#";
-            string[] fullnames = Directory.GetFiles(path,"*.txt");
-                foreach (string fullname in fullnames)
+            string path = @"D:\hello c#\new folder\folders\";
+            string[] folders = { "bin", "obj","sfsafsd" };
+
+            foreach (string folder in folders)
+            {
+                string folderPath = Path.Combine(path, folder);
+                if (Directory.Exists(folderPath))
                 {
-                    Console.WriteLine(fullname);
+                    Directory.Delete(folderPath, true);
+                    Console.WriteLine("folderPath is deleting");
                 }
+                else
+                {
+                    Console.WriteLine("there are no folders which you search");
+                }
+            }
+         
+            
+            
            
+            
         }
+        
+        
     }
 }
